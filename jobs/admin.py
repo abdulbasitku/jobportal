dd2efdad560fd720cabc11707f07459174c94a88
Abdul Basit Aziz <aaziz@corp.contoursoftware.net>
2019-02-21 14:17:19 +0500

    Company model registered

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Country, City, Job
+from .models import Country, City, Job, Company
 
 admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Job)
+admin.site.register(Company)
